9e8985e572cf814b10a03909eb16634aa7535f6d
mingttong <zhouwunan@qq.com>
2016-09-30 13:17:32 +0800

    test

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,16 @@
  * Created by lenovo on 2016/9/21.
  */
 
+window.onload = function () {
+
+    var p = document.querySelector(".attract .unit.content p");
+
+    var style = window.getComputedStyle(p, null);
+
+    var sizeText = style.fontSize;
+
+    var textNode = document.createTextNode(" font-size: " + sizeText);
+
+    p.appendChild(textNode);
+
+};
