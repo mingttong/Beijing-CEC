5f94d4c0985fb4dfa6b45377b77729b87a0a11ce
mingttong <zhouwunan@qq.com>
2016-10-01 11:23:59 +0800

    初步完成

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,3 @@
 /**
  * Created by lenovo on 2016/9/21.
  */
-
-window.onload = function () {
-
-    var p = document.querySelector(".attract .unit.content p");
-
-    var style = window.getComputedStyle(p, null);
-
-    var sizeText = style.fontSize;
-
-    var textNode = document.createTextNode(" font-size: " + sizeText);
-
-    p.appendChild(textNode);
-
-};
